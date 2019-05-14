807ed398582fa5d2a6b2c197059a6ea8021a133b
llupRisingll <luisedward.miranda@gmail.com>
2019-05-15 04:31:09 +0800

    login algorithm

--- user/api/login.php
+++ user/api/login.php
@@ -1,4 +1,6 @@
 <?php
+
+header('Access-Control-Allow-Origin: *');	
 include "../../includes/db.php";
 
 @$usn = $_POST["usn"];
--- user/do-not-touch/src/LoginForm.jsx
+++ user/do-not-touch/src/LoginForm.jsx
@@ -1,41 +1,51 @@
 import React from 'react'
 import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
 
-const LoginForm = () => (
-	<div className='login-form'>
-		{/*
-      Heads up! The styles below are necessary for the correct render of this example.
-      You can do same with CSS, the main idea is that all the elements up to the `Grid`
-      below must have a height of 100%.
-    */}
-		<style>{`
+function tryLogin(username, password){
+	let formData = new FormData();
+	formData.append('usn', 'admin');
+	formData.append('password', 'admin');
+
+	fetch(`//${window.location.hostname}/user/api/login.php`,
+		{
+			body: formData,
+			method: "post"
+		}).then(response => response.json())
+			.then(jsondata => console.log(jsondata))
+}
+
+function LoginForm() {
+	return (
+		<div className='login-form'>
+			<style>{`
       body > div,
       body > div > div,
       body > div > div > div.login-form {
         height: 100%;
       }
     `}
-		</style>
-		<Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
-			<Grid.Column style={{ maxWidth: 350 }}>
-				<Segment>
-					<Image src={require("./img/IMG_20190509_114203.jpg")} />
-					<Form size='large'>
-						<Form.Input fluid icon='user' iconPosition='left' placeholder='Username' />
-						<Form.Input
-							fluid
-							icon='lock'
-							iconPosition='left'
-							placeholder='Password'
-							type='password'
-						/>
+			</style>
+			<Grid textAlign='center' style={{height: '100%'}} verticalAlign='middle'>
+				<Grid.Column style={{maxWidth: 350}}>
+					<Segment>
+						<Image src={require("./img/IMG_20190509_114203.jpg")}/>
+						<Form size='large'>
+							<Form.Input fluid icon='user' iconPosition='left' placeholder='Username'/>
+							<Form.Input
+								fluid
+								icon='lock'
+								iconPosition='left'
+								placeholder='Password'
+								type='password'
+							/>
 
-						<Button color='blue' fluid size='small'>Login </Button>
-					</Form>
-				</Segment>
-			</Grid.Column>
-		</Grid>
-	</div>
-);
+							<Button color='blue' fluid size='small'>Login </Button>
+						</Form>
+					</Segment>
+				</Grid.Column>
+			</Grid>
+		</div>
+	);
+};
 
 export default LoginForm
