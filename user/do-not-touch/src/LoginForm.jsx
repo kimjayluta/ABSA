a3aab1a421f036819702114bc7dfc7234bc0f54c
llupRisingll <luisedward.miranda@gmail.com>
2019-05-15 04:40:32 +0800

    working login api

--- user/do-not-touch/src/LoginForm.jsx
+++ user/do-not-touch/src/LoginForm.jsx
@@ -1,51 +1,79 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
 
-function tryLogin(username, password){
-	let formData = new FormData();
-	formData.append('usn', 'admin');
-	formData.append('password', 'admin');
 
-	fetch(`//${window.location.hostname}/user/api/login.php`,
-		{
-			body: formData,
-			method: "post"
-		}).then(response => response.json())
-			.then(jsondata => console.log(jsondata))
-}
+class LoginForm extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			username: "",
+			password: ""
+		};
 
-function LoginForm() {
-	return (
-		<div className='login-form'>
-			<style>{`
+		this.handleChangeUsername = this.handleChangeUsername.bind(this);
+		this.handleChangePassword = this.handleChangePassword.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	tryLogin({username, password}){
+		let formData = new FormData();
+		formData.append('usn', username);
+		formData.append('password', password);
+
+		fetch(`//${window.location.hostname}/user/api/login.php`,
+			{
+				body: formData,
+				method: "post"
+			}).then(response => response.json())
+			.then(jsondata => console.log(jsondata));
+	}
+
+	handleChangeUsername(e) {
+		this.setState({ username: e.target.value });
+	}
+
+	handleChangePassword(e) {
+		this.setState({ password: e.target.value });
+	}
+
+	handleSubmit(e){
+		this.tryLogin(this.state)
+	}
+
+	render() {
+		return (
+			<div className='login-form'>
+				<style>{`
       body > div,
       body > div > div,
       body > div > div > div.login-form {
         height: 100%;
       }
     `}
-			</style>
-			<Grid textAlign='center' style={{height: '100%'}} verticalAlign='middle'>
-				<Grid.Column style={{maxWidth: 350}}>
-					<Segment>
-						<Image src={require("./img/IMG_20190509_114203.jpg")}/>
-						<Form size='large'>
-							<Form.Input fluid icon='user' iconPosition='left' placeholder='Username'/>
-							<Form.Input
-								fluid
-								icon='lock'
-								iconPosition='left'
-								placeholder='Password'
-								type='password'
-							/>
+				</style>
+				<Grid textAlign='center' style={{height: '100%'}} verticalAlign='middle'>
+					<Grid.Column style={{maxWidth: 350}}>
+						<Segment>
+							<Image src={require("./img/IMG_20190509_114203.jpg")}/>
+							<Form size='large' onSubmit={this.handleSubmit}>
+								<Form.Input fluid icon='user' iconPosition='left' onKeyUp={this.handleChangeUsername} placeholder='Username'/>
+								<Form.Input
+									fluid
+									icon='lock'
+									iconPosition='left'
+									placeholder='Password'
+									type='password'
+									onKeyUp={this.handleChangePassword}
+								/>
 
-							<Button color='blue' fluid size='small'>Login </Button>
-						</Form>
-					</Segment>
-				</Grid.Column>
-			</Grid>
-		</div>
-	);
-};
+								<Button color='blue' fluid size='small'>Login </Button>
+							</Form>
+						</Segment>
+					</Grid.Column>
+				</Grid>
+			</div>
+		);
+	}
+}
 
 export default LoginForm
