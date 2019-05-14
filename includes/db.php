c888b7438e7f88d6ab73c59403beb4ea854b8aea
llupRisingll <luisedward.miranda@gmail.com>
2019-05-15 02:18:00 +0800

    ignore files

--- includes/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-// to get the databse connection
-$conn = mysqli_connect('localhost','root','','absa');
-
-// Checker kung may error
-if(mysqli_connect_error()){
-    echo "Failed to connect into database " . mysqli_connect_error();
-}
-?>
