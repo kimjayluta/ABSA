b1abd5d89a795c908d435dfda04e5da2bd09a0bc
llupRisingll <luisedward.miranda@gmail.com>
2019-05-17 10:20:45 +0800

    refactored algorithm for the results of elimination

--- admin/result.php
+++ admin/result.php
@@ -1,6 +1,64 @@
 <?php
 $tourID = $_GET["tourID"];
 include("../includes/db.php");
+
+
+
+// Get all of the schedules for the final according to the current tournament
+$sql = "SELECT `teamone_id`, `teamtwo_id` FROM `schedule` WHERE `game_type`='elimination' AND `tour_id`='$tourID'";
+$result = mysqli_query($conn,$sql);
+
+$teamsToDisplay = array(); // List of teams but can have duplicate values
+while ($row = mysqli_fetch_assoc($result)){
+	$data[] = $row;
+	array_push($teamsToDisplay, $row["teamone_id"]);
+	array_push($teamsToDisplay, $row["teamtwo_id"]);
+}
+
+$gamesPerTeam =  array_count_values($teamsToDisplay);
+$uniqueTeamsToDisplay = array_unique($teamsToDisplay);
+
+// Loop the teams to get the players according to the teams within the schedules
+$allPlayersData = array();
+foreach($uniqueTeamsToDisplay as $team) {
+	$sql = "
+	SELECT 
+		CONCAT(p.`first_name`, ' ', p.`last_name`) AS name, p.`position`, p.`jersey_num`, p.`team_id`,
+		
+		((SUM(si.`free_throw`)+SUM(si.`two_points`)+SUM(si.`three_points`)) /  (SUM(si.`free_throw`)+SUM(si.`two_points`)+SUM(si.`three_points`) + SUM(si.`free_throw_missed`)+SUM(si.`two_points_missed`)+SUM(si.`three_points_missed`))) as field_goal,
+		
+		(SUM(si.`free_throw`)+SUM(si.`two_points`)+SUM(si.`three_points`)) /$gamesPerTeam[$team] as ppg,
+		
+		((SUM(si.`three_points`)) /  (SUM(si.`three_points`)+SUM(si.`three_points_missed`))) as three_fg,
+		
+		((SUM(si.`free_throw`)) /  (SUM(si.`free_throw`)+SUM(si.`free_throw_missed`))) as free_fg,
+		
+		((SUM(si.`o_rebound`)+SUM(si.`d_rebound`)) /$gamesPerTeam[$team]) as rfg,
+		((SUM(si.`o_rebound`)) /$gamesPerTeam[$team]) as orfg,
+		((SUM(si.`d_rebound`)) /$gamesPerTeam[$team]) as drfg,
+		((SUM(si.`blocks`)) /$gamesPerTeam[$team]) as bfg,
+		((SUM(si.`steals`)) /$gamesPerTeam[$team]) as sfg,
+		((SUM(si.`assist`)) /$gamesPerTeam[$team]) as afg,
+		((SUM(si.`foul`)) /$gamesPerTeam[$team]) as ffg,
+		((SUM(si.`turn_over`)) /$gamesPerTeam[$team]) as tfg
+		FROM `players` p
+	    INNER JOIN `score_info` si ON (si.`player_id` = p.`id`)
+	    
+	WHERE p.`tour_id`='$tourID' AND p.`team_id`='$team'
+	
+  	GROUP BY si.`player_id`
+	";
+
+	$result = mysqli_query($conn,$sql);
+
+	$data = array();
+	while ($row = mysqli_fetch_assoc($result)){
+		$data[] = $row;
+	}
+
+	@ array_push($allPlayersData, ...$data);
+}
+?>
 ?>
 
 <!DOCTYPE html>
@@ -95,9 +153,7 @@ include("../includes/db.php");
     <table class="table table-striped text-center" id="team-table">
         <thead>
             <tr>
-                <th scope="col">#</th>
-                <th scope="col">First Name</th>
-                <th scope="col">Last Name</th>
+                <th scope="col">Name</th>
                 <th scope="col">Position</th>
                 <th scope="col">Team</th>
                 <th scope="col">PPG</th>
@@ -115,104 +171,30 @@ include("../includes/db.php");
             </tr>
         </thead>
         <tbody>
-            <?php
-              $counter = 0;
-              $playerctr = 0;
-              $final = array();
-              $result = array("id" => 0, "ft" => 0, "ftm" => 0, "2pts" => 0, "2ptsm" => 0, "3pts" => 0, "3ptsm" => 0, "blk" =>0, "stl" => 0, "ast" => 0, "oreb" => 0, "dreb" => 0, "foul" => 0, "to" => 0 );
 
-            for($i = 1; $i<100;$i++){
-//                 $sql = "SELECT * FROM score_info WHERE tour_id = '$tourID' AND player_id = '$i'";
-                 $sql = "SELECT * FROM `score_info` si  INNER JOIN `schedule` s ON(si.sched_id = s.id) WHERE s.`tour_id` = '$tourID' AND si.`player_id` = '$i'";
-//                 echo $sql;
-//                 exit;
-                 $select = mysqli_query($conn,$sql);
-                 if(mysqli_num_rows($select)>0) {
-                    $playerctr++;
-                    while($row = mysqli_fetch_array($select)){
-                        $result['ft'] = $result['ft'] + $row["free_throw"];
-                        $result['ftm'] = $result['ftm'] + $row["free_throw_missed"];
-                        $result['2pts'] = $result['2pts'] + $row["two_points"];
-                        $result['2ptsm'] = $result['2ptsm'] + $row["two_points_missed"];
-                        $result['3pts'] = $result['3pts'] + $row["three_points"];
-                        $result['3ptsm'] = $result['3ptsm'] + $row["three_points_missed"];
-                        $result['blk'] = $result['blk'] + $row["blocks"];
-                        $result['stl'] = $result['stl'] + $row["steals"];
-                        $result['ast'] = $result['ast'] + $row["assist"];
-                        $result['oreb'] = $result['oreb'] + $row["o_rebound"];
-                        $result['dreb'] = $result['dreb'] + $row["d_rebound"];
-                        $result['foul'] = $result['foul'] + $row["foul"];
-                        $result['to'] = $result['to'] + $row["turn_over"];
-
-                        $counter++;
-                    }
-
-                    $total2 = $result['2pts'] * 2;
-                    $total3 = $result['3pts'] * 3;
-                    $total_points = $result['ft'] + $total2 + $total3;
-                    $total_points_ave  = (@($total_points / $counter));
-                    $total_fg = (@($result['2pts'] + $result['2ptsm'] + $result['3pts'] + $result['3ptsm'])/($result['2pts'] + $result['3pts'] ))*100;
-                    $total_fg_ave = (@($total_fg/$counter));
-                    $tpts =  (@(($result['3pts']+$result['3ptsm'])/$result['3pts'])*100);
-                    $tptsfg =  (@($tpts/$counter));
-                    $ft =  (@(($result['ft']+$result['ftm'])/$result['ft'])*100);
-                    $total_ft_ave = (@($ft/$counter));
-                    $rbpg = $result['oreb']+$result['dreb'];
-                    $to = (@($result['to']/$counter));
-
-
-                    array_push($final,array(
-                              "ID"             => $i,
-                              "PPG"            => round($total_points_ave,2),
-                              "FG"             => round($total_points_ave,2),
-                              "3PT_FG"         => round($tptsfg,2),
-                              "Free_Throw_FG"  => round($total_ft_ave,2),
-                              "RPG"            => round($rbpg,2),
-                              "OffRPG"         => round($result['oreb'],2),
-                              "DefRPG"         => round($result['dreb'],2),
-                              "BPG"            => round($result['blk'],2),
-                              "SPG"            => round($result['stl'],2),
-                              "APG"            => round($result['ast'],2),
-                              "FPG"            => round($result['foul'],2),
-                              "TOPG"           => round($to,2)
-                        )
-                      );
-                }else{
-                    continue;
-                }
-            }
-
-
-
-            for($j = 0;$j<$playerctr;$j++){
-                  $select_player = mysqli_query($conn,"SELECT * FROM `players` WHERE `id` = '".$final[$j]['ID']."'");
-                  $row = mysqli_fetch_array($select_player);
-                  //$select_team = mysqli_query($conn,"SELECT * FROM `teams` WHERE `id` = '".$row['team_id']."'");
-                  //$row2 = mysqli_fetch_array($select_team);
-                     echo "
-                     <tr>
-                      <th scope='row'>".$j."</th>
-                      <td>".$row['first_name']."</td>
-                      <td>".$row['last_name']."</td>
-                      <td>".$row['position']."</td>
-                      <td>team name</td>
-                      <td>".$final[$j]['PPG']."</td>
-                      <td>".$final[$j]['FG']."</td>
-                      <td>".$final[$j]['3PT_FG']."</td>
-                      <td>".$final[$j]['Free_Throw_FG']."</td>
-                      <td>".$final[$j]['RPG']."</td>
-                      <td>".$final[$j]['OffRPG']."</td>
-                      <td>".$final[$j]['DefRPG']."</td>
-                      <td>".$final[$j]['BPG']."</td>
-                      <td>".$final[$j]['SPG']."</td>
-                      <td>".$final[$j]['APG']."</td>
-                      <td>".$final[$j]['FPG']."</td>
-                      <td>".$final[$j]['TOPG']."</td>
-                    </tr>";
-            }
-
-
-            ?>
+        <?php
+        foreach ($allPlayersData as $row){
+	        echo "
+	            <tr>
+	                <td>". $row["name"] ."</td>
+	                <td>". $row["position"] ."</td>
+	                <td>". $row["team_id"] ."</td>
+	                <td>". round($row["ppg"],2). "</td>
+	                <td>". round($row["field_goal"]*100,2). "%</td>
+	                <td>". round($row["three_fg"]*100,2). "%</td>
+	                <td>". round($row["free_fg"]*100,2). "%</td>
+	                <td>". round($row["rfg"],2). "</td>
+	                <td>". round($row["orfg"],2). "</td>
+	                <td>". round($row["drfg"],2). "</td>
+	                <td>". round($row["bfg"],2). "</td>
+	                <td>". round($row["sfg"],2). "</td>
+	                <td>". round($row["afg"],2). "</td>
+	                <td>". round($row["ffg"],2). "</td>
+	                <td>". round($row["tfg"],2). "</td>
+                </tr>
+				";
+        }
+        ?>
         </tbody>
   </table>
   </div>
